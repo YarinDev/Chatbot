e8159232d4d4ce81b8ea0f7f9177d8ed32fdd95f
YarinDev <yarin.software@gmail.com>
2024-03-05 15:23:10 +0200

    added 2 options in IMDb - search movie | display top 250

--- src/main/java/com/handson/chatbot/controller/BotController.java
+++ src/main/java/com/handson/chatbot/controller/BotController.java
@@ -1,24 +1,35 @@
 package com.handson.chatbot.controller;
 
 import com.handson.chatbot.service.AmazonService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 
-@Service
-@RestController
+@Api(tags = "IMDB Service")
+@Controller
 @RequestMapping("/bot")
 public class BotController {
 
     @Autowired
-    AmazonService amazonService;
+    private AmazonService amazonService;
 
-    @RequestMapping(value = "/amazon", method = RequestMethod.GET)
-    public ResponseEntity<?> getProduct(@RequestParam String keyword) throws IOException {
-        return new ResponseEntity<>(amazonService.searchProducts(keyword), HttpStatus.OK);
+    @ApiOperation(value = "Get top 250 movies from IMDb")
+    @RequestMapping(value = "/IMDB/top250", method = RequestMethod.GET)
+    public ResponseEntity<?> getTopMovies() throws IOException {
+        String topMovies = amazonService.searchTopMovies(null);
+        return new ResponseEntity<>(topMovies, HttpStatus.OK);
+    }
+
+    @ApiOperation(value = "Search movies on IMDb")
+    @RequestMapping(value = "/IMDB/search", method = RequestMethod.GET)
+    public ResponseEntity<?> searchMovie(@RequestParam String keyword) throws IOException {
+        String movieDetails = amazonService.searchMovie(keyword);
+        return new ResponseEntity<>(movieDetails, HttpStatus.OK);
     }
 }
--- src/main/java/com/handson/chatbot/service/AmazonService.java
+++ src/main/java/com/handson/chatbot/service/AmazonService.java
@@ -9,23 +9,26 @@ import java.util.regex.Pattern;
 
 @Service
 public class AmazonService {
-    public static final Pattern PRODUCT_PATTERN = Pattern.compile("tabindex=\"0\"><h3 class=\"ipc-title__text\">(\\d+\\.[^<]+)<\\/h3>.*?aria-label=\"IMDb rating: (\\d+\\.\\d+)\"");
+    // This PRODUCT_PATTERN is used compile the regex pattern to get the movie name and rating
+    public static final Pattern MOVIE250_PATTERN = Pattern.compile("tabindex=\"0\"><h3 class=\"ipc-title__text\">(\\d+\\.[^<]+)<\\/h3>.*?aria-label=\"IMDb rating: (\\d+\\.\\d+)\"");
 
-    public String searchProducts(String keyword) throws IOException {
-        return parseProductHtml(getProductHtml(keyword));
+    public String searchTopMovies(String keyword) throws IOException {
+        return parseTopMoviesHtml(getTopMoviesHtml(keyword));
     }
 
-    private String parseProductHtml(String html) {
+    // This parseProductHtml method is used to parse the HTML of the movie page using regex to get the movie name and rating
+    private String parseTopMoviesHtml(String html) {
         String res = "";
-        Matcher matcher = PRODUCT_PATTERN.matcher(html);
+        Matcher matcher = MOVIE250_PATTERN.matcher(html);
+        // This while loop is used to find the movie name and rating and append it to the res string
         while (matcher.find()) {
-            res += matcher.group(1) + "\nRating: " + matcher.group(2)  + "\n";
+            res += matcher.group(1) + "\nRating: " + matcher.group(2) + "\n";
         }
         return res;
     }
 
-    // This getProductHtml method is used to get the HTML of the movie page from imdb
-    private String getProductHtml(String keyword) throws IOException {
+    // This getTopMoviesHtml method is used to get the HTML of the top 250 movies page from imdb
+    private String getTopMoviesHtml(String keyword) throws IOException {
         OkHttpClient client = new OkHttpClient().newBuilder()
                 .build();
         MediaType mediaType = MediaType.parse("text/plain");
@@ -39,6 +42,54 @@ public class AmazonService {
                 .addHeader("cache-control", "max-age=0")
                 .addHeader("cookie", "session-id=142-1182356-4192552; session-id-time=2082787201l; ad-oo=0; ubid-main=133-3401308-8830412; uu=eyJpZCI6InV1Yzk4NjMzZmRjZjI2NDg0YWEyNjUiLCJwcmVmZXJlbmNlcyI6eyJmaW5kX2luY2x1ZGVfYWR1bHQiOmZhbHNlfX0=; session-token=S/CQA+17uEbNASKusNKaaus72IHJ9mfIDPEl+mqn4JkjX/CvupgdzMh1W1mlHTc7P3p+THg0Fg73PrT22WS9vUau6XdAzVG7ByhBrf3twX/J5LJE3UC1qyEnvK186G2ANwME23vQ7oP6ACKA1qC/2E42jPqo8N6+d1cicBDAqZ4Xxu/hPCngaW2QC8W3rgDluTrCM9jVLCu32yk84ksCquSssCwpBSNUHhrh49vm3ix1G7nhNYVMy50RHsnv+rBSM6q3EdgcXOzXrcMGlA6337lUR7IIYRix7fsJuH2/Rzi5yv4ylqoLnjKvHNdQ7pYTS47+6DKJeTnjl+qyC14N3cWkT54o/k/e; csm-hit=tb:8C2G9PPHZD8XN0RR0764+s-8C2G9PPHZD8XN0RR0764|1709559862642&t:1709559862642&adb:adblk_no; ci=e30; session-id=142-1182356-4192552; session-id-time=2082787201l; session-token=S/CQA+17uEbNASKusNKaaus72IHJ9mfIDPEl+mqn4JkjX/CvupgdzMh1W1mlHTc7P3p+THg0Fg73PrT22WS9vUau6XdAzVG7ByhBrf3twX/J5LJE3UC1qyEnvK186G2ANwME23vQ7oP6ACKA1qC/2E42jPqo8N6+d1cicBDAqZ4Xxu/hPCngaW2QC8W3rgDluTrCM9jVLCu32yk84ksCquSssCwpBSNUHhrh49vm3ix1G7nhNYVMy50RHsnv+rBSM6q3EdgcXOzXrcMGlA6337lUR7IIYRix7fsJuH2/Rzi5yv4ylqoLnjKvHNdQ7pYTS47+6DKJeTnjl+qyC14N3cWkT54o/k/e")
                 .addHeader("referer", "https://www.google.com/")
+                .addHeader("sec-ch-ua", "\"Not_A Brand\";v=\"99\", \"Google Chrome\";v=\"109\", \"Chromium\";v=\"109\"")
+                .addHeader("sec-ch-ua-mobile", "?0")
+                .addHeader("sec-ch-ua-platform", "\"Linux\"")
+                .addHeader("sec-fetch-dest", "document")
+                .addHeader("sec-fetch-mode", "navigate")
+                .addHeader("sec-fetch-site", "same-origin")
+                .addHeader("sec-fetch-user", "?1")
+                .addHeader("upgrade-insecure-requests", "1")
+                .addHeader("user-agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36")
+                .build();
+        Response response = client.newCall(request).execute();
+        return response.body().string();
+    }
+
+
+    // This PRODUCT_PATTERN is used compile the regex pattern to get the movie name and rating
+    public static final Pattern MOVIE_SEARCH_PATTERN = Pattern.compile("\"titleNameText\":([^,]+),\"titleReleaseText\":\"([^\"]+)\".*?\"topCredits\":\\[(.*?)\\]");
+
+    public String searchMovie(String keyword) throws IOException {
+        return parseMovieHtml(getMovieSearchHtml(keyword));
+    }
+
+    // This parseProductHtml method is used to parse the HTML of the movie page using regex to get the movie name and rating
+    private String parseMovieHtml(String html) {
+        String res = "";
+        Matcher matcher = MOVIE_SEARCH_PATTERN.matcher(html);
+        // This while loop is used to find the movie name and rating and append it to the res string
+        while (matcher.find()) {
+            res += "Movie name: " + matcher.group(1) + " ,Year:" + matcher.group(2) + " ,Top Credits: " + matcher.group(3) + "\n";
+        }
+        return res;
+    }
+
+    // This getTopMoviesHtml method is used to get the HTML of the top 250 movies page from imdb
+    private String getMovieSearchHtml(String keyword) throws IOException {
+        OkHttpClient client = new OkHttpClient().newBuilder()
+                .build();
+        MediaType mediaType = MediaType.parse("text/plain");
+        RequestBody body = RequestBody.create(mediaType, "");
+        Request request = new Request.Builder()
+                .url("https://www.imdb.com/find/?q=" + keyword + "&ref_=nv_sr_sm")
+                .method("GET", null)
+                .addHeader("authority", "www.imdb.com")
+                .addHeader("accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9")
+                .addHeader("accept-language", "en-US,en;q=0.9")
+                .addHeader("cache-control", "max-age=0")
+                .addHeader("cookie", "session-id=142-1182356-4192552; session-id-time=2082787201l; ad-oo=0; ubid-main=133-3401308-8830412; uu=eyJpZCI6InV1Yzk4NjMzZmRjZjI2NDg0YWEyNjUiLCJwcmVmZXJlbmNlcyI6eyJmaW5kX2luY2x1ZGVfYWR1bHQiOmZhbHNlfX0=; session-token=XBtnJAZfyaGIkaotjE5a01RuQeSzQxV7n77JN3tgoIWEUHrWNTqhfCJv4iOfV83cB3vYieV0Wk9C8DiwyiIo2dXxhbvh5AxUYlVT44cMk3uhYUt/h00gTOJsfIanAVsx8+2DJ0JVcp+wyTShdR40bGbbM6dwOei3ESV/xRnbZYc/1geLfyj81eGUgXycYXqEp+kqoVEMC3CfxGIugsbFwyVv/Auq8EVWnm1Yem0CGhrtwMwhytqFhXpqJ5LHWo7bZOanluUUFnRrk5nfLJ+zQHiGIITQlC8w1pOrRsEIoneHp6kRM3O0N5e5tnlqAEWT0fm4LrAw8rcCooOo6JS+/6yg+awWVeR9; ci=e30; csm-hit=tb:s-59G7P67NZ9RYP1J4N9YY|1709634357305&t:1709634357635&adb:adblk_no; session-id=142-1182356-4192552; session-id-time=2082787201l; session-token=XBtnJAZfyaGIkaotjE5a01RuQeSzQxV7n77JN3tgoIWEUHrWNTqhfCJv4iOfV83cB3vYieV0Wk9C8DiwyiIo2dXxhbvh5AxUYlVT44cMk3uhYUt/h00gTOJsfIanAVsx8+2DJ0JVcp+wyTShdR40bGbbM6dwOei3ESV/xRnbZYc/1geLfyj81eGUgXycYXqEp+kqoVEMC3CfxGIugsbFwyVv/Auq8EVWnm1Yem0CGhrtwMwhytqFhXpqJ5LHWo7bZOanluUUFnRrk5nfLJ+zQHiGIITQlC8w1pOrRsEIoneHp6kRM3O0N5e5tnlqAEWT0fm4LrAw8rcCooOo6JS+/6yg+awWVeR9")
+                .addHeader("referer", "https://www.imdb.com/")
                 .addHeader("sec-ch-ua", "\"Not_A Brand\";v=\"99\", \"Google Chrome\";v=\"109\", \"Chromium\";v=\"109\"")
                 .addHeader("sec-ch-ua-mobile", "?0")
                 .addHeader("sec-ch-ua-platform", "\"Linux\"")
